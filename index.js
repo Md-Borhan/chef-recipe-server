4cb45bf8e5699a515a3ee84945a476cf53d165dd
Borhan Uddin <borhanm373@gmail.com>
2023-05-03 00:53:18 +0600

    ingredients data updated

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.get("/recipes", (req, res) => {
   res.send(recipeData);
 });
 
+app.get("/recipes/:id", (req, res) => {
+  const { id } = req.params;
+  const existId = recipeData.find((sr) => sr.id == id);
+  res.send(existId);
+});
+
 app.listen(port, () => {
   console.log(`Assignment 10 server is running PORT ${port}`);
 });
